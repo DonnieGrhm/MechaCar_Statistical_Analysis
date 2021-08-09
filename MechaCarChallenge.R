a35ea2aa2cdcab8a97377c0c5f8bda07c42c6c56
Donald Graham <donaldg1994@yahoo.com>
2021-08-09 00:31:28 -0400

    summary statistics on suspension

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -5,3 +5,11 @@ MC_mpg<- read.csv('MechaCar_mpg.csv')
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MC_mpg)
 
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MC_mpg))
+
+#-------------------
+
+coil<- read.csv('Suspension_Coil.csv')
+
+total_summary<- coil %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
+
+lot_summary<- coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
