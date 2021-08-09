197a8905de75eee0bc88312932c28c0ba83cfe9b
Donald Graham <donaldg1994@yahoo.com>
2021-08-08 23:51:04 -0400

    multiple linear module of mpg

---
+++ MechaCarChallenge.R
@@ -0,0 +1,7 @@
+library(dplyr)
+
+MC_mpg<- read.csv('MechaCar_mpg.csv')
+
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MC_mpg)
+
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MC_mpg))
